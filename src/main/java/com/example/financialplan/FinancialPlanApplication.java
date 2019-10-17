fb41c830c93588ceef8a6b4955ed53d220df99cd
VSXXMAK <mateusz.klepacki@vps.no>
2019-10-17 17:51:28 +0200

    first commit

---
+++ src/main/java/com/example/financialplan/FinancialPlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.financialplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialPlanApplication.class, args);
+    }
+
+}
