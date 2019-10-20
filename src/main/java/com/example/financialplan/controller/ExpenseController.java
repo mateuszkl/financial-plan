a010055344e2f73b6b920e6aa2217c8a61acfc5a
mateuszkl <mateusz.klepacki1@gmail.com>
2019-10-20 16:42:35 +0200

    added expense edit form

--- src/main/java/com/example/financialplan/controller/ExpenseController.java
+++ src/main/java/com/example/financialplan/controller/ExpenseController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/expenses")
@@ -39,6 +41,28 @@ public class ExpenseController {
         expenseRepository.save(expense);
         model.addAttribute(EXPENSES_ATTRIBUTE, expenseRepository.findAll());
 
+        return "index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable("id") Long id, Model model) {
+        Expense expense = expenseRepository.findById(id)
+                .orElseThrow(IllegalArgumentException::new);
+        model.addAttribute("expense", expense);
+        return "editExpense";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String saveEdit(@Valid Expense expense, @PathVariable("id") Long id, BindingResult result, Model model) {
+        expense.setId(id);
+
+        if (result.hasErrors()) {
+            model.addAttribute("expense", expense);
+            return "editExpense";
+        }
+
+        expenseRepository.save(expense);
+        model.addAttribute(EXPENSES_ATTRIBUTE, expenseRepository.findAll());
         return "index";
     }
 }
